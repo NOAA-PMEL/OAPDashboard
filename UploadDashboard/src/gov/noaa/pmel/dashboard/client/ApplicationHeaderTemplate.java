3a8cf9ee94644ec06c4f835c6512592e38a42681
linus.kamb <Linus.Kamb@noaa.gov>
2019-09-19 14:29:13 -0700

    Rather hackish support for sending feedback and changing user password.

--- UploadDashboard/src/gov/noaa/pmel/dashboard/client/ApplicationHeaderTemplate.java
+++ UploadDashboard/src/gov/noaa/pmel/dashboard/client/ApplicationHeaderTemplate.java
@@ -9,12 +9,22 @@ import java.util.logging.Logger;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.MouseOutEvent;
+import com.google.gwt.event.dom.client.MouseOutHandler;
+import com.google.gwt.event.dom.client.MouseOverEvent;
+import com.google.gwt.event.dom.client.MouseOverHandler;
 import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.Timer;
+import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.InlineLabel;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.MouseListener;
 import com.google.gwt.user.client.ui.Widget;
 
 import gov.noaa.pmel.dashboard.shared.DashboardDatasetList;
@@ -29,12 +39,16 @@ public class ApplicationHeaderTemplate extends CompositeWithUsername {
     @UiField
     Label titleLabel;
     @UiField
+    Anchor sendFeedbackButton;
+    @UiField
     InlineLabel userInfoLabel;
     @UiField
     Button logoutButton;
+    @UiField Image askChangePasswordBtn;
+    @UiField FlowPanel askPasswdChangePnl;
+    @UiField Button changePasswordBtn;
     
     private HandlerRegistration logoutHandler;
-
     
     interface ApplicationHeaderTemplateUiBinder extends UiBinder<Widget, ApplicationHeaderTemplate> {
     }
@@ -43,20 +57,97 @@ public class ApplicationHeaderTemplate extends CompositeWithUsername {
 
     public ApplicationHeaderTemplate() {
         initWidget(uiBinder.createAndBindUi(this));
+        hideChangePasswordOption();
 		logoutButton.setText(LOGOUT_TEXT);
 		logoutButton.setTitle(LOGOUT_TEXT);
+        askChangePasswordBtn.addClickHandler(new ClickHandler() {
+            @Override
+            public void onClick(ClickEvent evt) {
+                GWT.log("Pressed ask change password");
+                showChangePasswordOption();
+            }
+        });
+        askChangePasswordBtn.addMouseOverHandler(new MouseOverHandler() {
+            @Override
+            public void onMouseOver(MouseOverEvent arg0) {
+                showChangePasswordOption();
+            }
+        });
+        askChangePasswordBtn.addMouseOutHandler(new MouseOutHandler() {
+            @Override
+            public void onMouseOut(MouseOutEvent arg0) {
+                delayHideChangePasswordOption();
+            }
+        });
+        changePasswordBtn.addMouseOverHandler(new MouseOverHandler() {
+            @Override
+            public void onMouseOver(MouseOverEvent arg0) {
+                showChangePasswordOption();
+            }
+        });
+        changePasswordBtn.addMouseOutHandler(new MouseOutHandler() {
+            @Override
+            public void onMouseOut(MouseOutEvent arg0) {
+                delayHideChangePasswordOption();
+            }
+        });
+        changePasswordBtn.addClickHandler(new ClickHandler() {
+            @Override
+            public void onClick(ClickEvent evt) {
+                showChangePasswordPopup();
+            }
+        });
 		handleLogout();
+    }
+    private void showChangePasswordPopup() {
+        GWT.log("show change password popoupa");
+        hideChangePasswordOption();
+        UploadDashboard.showChangePasswordPopup();
+    }
+    private void showChangePasswordOption() {
+        GWT.log("show Style:"+askPasswdChangePnl.getStyleName());
+        cancelTimer();
+        askPasswdChangePnl.removeStyleName("slideOut");
+        askPasswdChangePnl.addStyleName("slideIn");
+    }
+    private void hideChangePasswordOption() {
+        GWT.log("hide Style:"+askPasswdChangePnl.getStyleName());
+        askPasswdChangePnl.removeStyleName("slideIn");
+        askPasswdChangePnl.addStyleName("slideOut");
+    }
+    private void cancelTimer() {
+        GWT.log("cancelTimer");
+        if ( hideTimer != null ) {
+            hideTimer.cancel();
+        }
+    }
+    Timer hideTimer = null;
+    private void delayHideChangePasswordOption() {
+        cancelTimer();
+        hideTimer = new Timer() {
+            @Override
+            public void run() {
+                hideChangePasswordOption();
+            }
+        };
+        hideTimer.schedule(300);
     }
 
     protected void setPageTitle(String title) {
         titleLabel.setText(title);
     }
     
-//    @UiHandler("logoutButton")
-    void logoutOnClick(ClickEvent event) {
+    static void logoutOnClick(ClickEvent event) {
         GWT.log("GWT log Header logout");
         logger.info("Logger Header logout");
         DashboardLogoutPage.showPage();
+    }
+
+    @UiHandler("sendFeedbackButton")
+    void sendFeedbackButtonOnClick(ClickEvent event) {
+        GWT.log("GWT log Header sendFeedback");
+        logger.info("Logger Header sendFeedback");
+        UploadDashboard.showFeedbackPopup();
     }
 
     public void setDatasetIds(String datasetIds) {
