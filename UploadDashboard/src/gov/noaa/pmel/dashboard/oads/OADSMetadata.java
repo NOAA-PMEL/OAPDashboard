10eac49a62a361643f94adaae3a96f6357d89e31
linus.kamb <Linus.Kamb@noaa.gov>
2019-09-19 15:20:46 -0700

    remove some unused imports.

--- UploadDashboard/src/gov/noaa/pmel/dashboard/oads/OADSMetadata.java
+++ UploadDashboard/src/gov/noaa/pmel/dashboard/oads/OADSMetadata.java
@@ -6,21 +6,18 @@ package gov.noaa.pmel.dashboard.oads;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.io.StringWriter;
 import java.io.Writer;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.validation.spi.ConfigurationState;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.jdom2.Document;
 
 import gov.noaa.pmel.dashboard.datatype.DashDataType;
 import gov.noaa.pmel.dashboard.dsg.StdUserDataArray;
