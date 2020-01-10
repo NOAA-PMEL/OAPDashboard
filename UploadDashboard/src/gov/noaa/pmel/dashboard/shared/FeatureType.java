152ed94bc415b497501176546a6120f30b30a2f2
linus.kamb <Linus.Kamb@noaa.gov>
2020-01-10 15:27:27 -0800

    Add FileType in addition to FeatureType

--- UploadDashboard/src/gov/noaa/pmel/dashboard/shared/FeatureType.java
+++ UploadDashboard/src/gov/noaa/pmel/dashboard/shared/FeatureType.java
@@ -8,11 +8,19 @@ package gov.noaa.pmel.dashboard.shared;
  *
  */
 public enum FeatureType {
-    UNSPECIFIED,
-    TIMESERIES,
-    TRAJECTORY,
-    PROFILE,
-    PROFILE_TIMESERIES, 
-    TRAJECTORY_PROFILE,
-    OTHER
+    UNSPECIFIED("unspecified"),
+    TIMESERIES("timeSeries"),
+    TRAJECTORY("trajectory"),
+    PROFILE("profile"),
+    TIMESERIES_PROFILE("timeSeriesProfile"), 
+    TRAJECTORY_PROFILE("trajectoryProfile"),
+    OTHER("other");
+    
+    String _dsgTypeName;
+    
+    FeatureType(String dsgTypeName) {
+        _dsgTypeName = dsgTypeName;
+    }
+    
+    public String dsgTypeName() { return _dsgTypeName; }
 }
