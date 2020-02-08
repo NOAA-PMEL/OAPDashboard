521b7200eed9486e87f91379242d4f53cd15d69f
linus.kamb <Linus.Kamb@noaa.gov>
2020-02-07 16:12:44 -0800

    Oops.  Wrong file for download archive package Data Services.

--- UploadDashboard/src/gov/noaa/pmel/dashboard/server/DataServices.java
+++ UploadDashboard/src/gov/noaa/pmel/dashboard/server/DataServices.java
@@ -1,31 +1,23 @@
 /**
  * 
  */
-package gov.noaa.pmel.dashboard.server.ws;
+package gov.noaa.pmel.dashboard.server;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URLConnection;
+import java.nio.file.Files;
 import java.util.Arrays;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
-import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.ResponseBuilder;
-import javax.ws.rs.core.Response.Status;
-import javax.ws.rs.core.SecurityContext;
 
 import org.apache.logging.log4j.Logger;
 
-import gov.noaa.pmel.dashboard.server.DashboardConfigStore;
-import gov.noaa.pmel.dashboard.shared.DashboardUtils;
 import gov.noaa.pmel.tws.util.StringUtils;
 
 import org.apache.logging.log4j.LogManager;
@@ -35,54 +27,54 @@ import org.apache.logging.log4j.LogManager;
  *
  */
 @Path("data")
-public class DataServices extends ResourceBase {
+public class DataServices extends HttpServlet {
 
     private static Logger logger = LogManager.getLogger(DataServices.class);
     
-    @Context HttpServletRequest httpRequest;
-    @Context HttpServletResponse httpResponse;
-	@Context ServletContext servletContext;
-	@Context SecurityContext securityContext;
-    
     // "tomcat/content/OAPUploadDashboard/ArchiveBundles/bags/29AH20110128/29AH20110128_20200203T070959Z/29AH20110128_bagit.zip");
     private static File bagRoot = null; 
-    
-    /**
-     * 
-     */
     public DataServices() {
         bagRoot = getBagRoot();
     }
 
-    @GET
-    @Path("{pckg}")
-    @Produces(MediaType.APPLICATION_OCTET_STREAM)
-    public Response downloadDataPackage(@PathParam("pckg") String p_pckg) {
-        Response response = null;
-        logger.debug(fullDump(httpRequest));
-        logger.debug("get data package for " + p_pckg +
-                     " from " + getRemoteAddress(httpRequest));
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        logger.debug(request.getRemoteAddr() + " : " + request);
+        System.out.println(request.getRemoteAddr() + " : " + request);
+    
+        String p_pckg = request.getPathInfo();
+        logger.debug("get data package for " + p_pckg + " from " + getRemoteAddress(request));
         if ( StringUtils.emptyOrNull(p_pckg)) {
-            return Response.status(Status.BAD_REQUEST).entity("Invalid package ID").build();
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid package ID");
+            return;
         }
         String ucPckg = p_pckg.toUpperCase();
         try {
             File pckgFile = getFileFor(ucPckg);
             if ( pckgFile != null && pckgFile.exists()) {
-                ResponseBuilder rb = Response.ok(pckgFile);
-                rb.header("Content-Disposition", "attachment;filename=" + pckgFile.getName());
-                response = rb.build();
+                String type = URLConnection.guessContentTypeFromName(pckgFile.getName());
+                response.setContentType(type);
+                response.setContentLengthLong(pckgFile.length());
+                response.setHeader("Content-Disposition", "attachment;filename=" + pckgFile.getName());
+                Files.copy(pckgFile.toPath(), response.getOutputStream());
             } else {
-                response = Response.status(Status.NOT_FOUND).entity("No data package found.  Perhaps the package hold time has expired.").build();
+                response.sendError(HttpServletResponse.SC_NOT_FOUND, "No data package found.  Perhaps the package hold time has expired.");
+                return;
             }
         } catch (Exception ex) {
             logger.warn(ex.getMessage(), ex);
-            response = Response.serverError().entity("An error occurred on the server. Please try again later.").build();
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "An error occurred on the server. Please try again later.");
         }
-        return response;
-        
     }
 
+    protected static String getRemoteAddress(HttpServletRequest httpRequest) {
+        String remoteAddr = httpRequest.getHeader("x-forwarded-for");
+        if ( remoteAddr == null ) {
+            remoteAddr = httpRequest.getRemoteAddr();
+        }
+        return remoteAddr;
+    }
+    
     static File getBagRoot() {
         File contentRoot = DashboardConfigStore.getAppContentDir();
         File baggy = new File(contentRoot, "ArchiveBundles/bags");
