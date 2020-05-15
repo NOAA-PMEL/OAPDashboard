cb6bc42bf52d2306769b54028ffb3fa55e9d1555
linus.kamb <Linus.Kamb@noaa.gov>
2020-05-15 16:48:34 -0700

    Fix for Firefox not logging out (saying "Connection used to make this request was not secure.")  Chrome reported request was cancelled, but still logout still occurred.

--- UploadDashboard/src/gov/noaa/pmel/dashboard/client/ApplicationHeaderTemplate.java
+++ UploadDashboard/src/gov/noaa/pmel/dashboard/client/ApplicationHeaderTemplate.java
@@ -80,25 +80,33 @@ public class ApplicationHeaderTemplate extends CompositeWithUsername {
     }
     
     void doLogout() {
-        GWT.log("GWT log Header logout: " + this);
         logger.info("Logger Header logout");
         UploadDashboard.closePopups();
+        try {
         UploadDashboard.getService().logoutUser(new OAPAsyncCallback<Void>() {
             @Override
             public void onSuccess(Void nada) {
+                UploadDashboard.logToConsole("Logout success");
                 Cookies.removeCookie("JSESSIONID");
                 UploadDashboard.stopHistoryHandling();
                 UploadDashboard.showAutoCursor();
+                Window.Location.assign("dashboardlogout.html");
             }
             @Override
             public void onFailure(Throwable ex) {
-                GWT.log("Logout error:" + ex.toString());
+                UploadDashboard.logToConsole("Logout error:" + ex.toString());
+//                Window.alert(String.valueOf(ex));
                 Cookies.removeCookie("JSESSIONID");
                 UploadDashboard.stopHistoryHandling();
                 UploadDashboard.showAutoCursor();
+                Window.Location.assign("dashboardlogout.html");
             }
         });
-        Window.Location.assign("dashboardlogout.html");
+        } catch (Throwable t) {
+            UploadDashboard.logToConsole("logout exception: "+ String.valueOf(t));
+//            Window.alert(String.valueOf(t));
+            Window.Location.assign("dashboardlogout.html");
+        }
     }
 
     static void doSendFeedback() {
