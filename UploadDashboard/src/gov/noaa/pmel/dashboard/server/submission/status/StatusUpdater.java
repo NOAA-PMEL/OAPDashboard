3a205fd25792f7acc9ac97e711b7b9d45af3bea1
linus.kamb <Linus.Kamb@noaa.gov>
2021-02-04 10:55:41 -0800

    Move updateStatus to StatusUpdater from StatusServices; move StatusUpdater to status package from util; add NOT_SUBMITTED status state.

--- UploadDashboard/src/gov/noaa/pmel/dashboard/server/submission/status/StatusUpdater.java
+++ UploadDashboard/src/gov/noaa/pmel/dashboard/server/submission/status/StatusUpdater.java
@@ -1,13 +1,21 @@
 /**
  * 
  */
-package gov.noaa.pmel.dashboard.server.util;
+package gov.noaa.pmel.dashboard.server.submission.status;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import gov.noaa.pmel.dashboard.server.Archive;
+import gov.noaa.pmel.dashboard.server.DashboardException;
+import gov.noaa.pmel.dashboard.server.DashboardServerUtils;
+import gov.noaa.pmel.dashboard.server.Users;
 import gov.noaa.pmel.dashboard.server.db.dao.DaoFactory;
 import gov.noaa.pmel.dashboard.server.db.dao.SubmissionsDao;
-import gov.noaa.pmel.dashboard.server.submission.status.StatusRecord;
-import gov.noaa.pmel.dashboard.server.submission.status.StatusState;
-import gov.noaa.pmel.dashboard.server.submission.status.SubmissionRecord;
+import gov.noaa.pmel.dashboard.server.model.User;
+import gov.noaa.pmel.dashboard.server.util.Notifications;
+import gov.noaa.pmel.dashboard.shared.NotFoundException;
+import gov.noaa.pmel.tws.util.ApplicationConfiguration;
 
 /**
  * @author kamb
@@ -27,6 +35,8 @@ public class StatusUpdater {
     private static String _status;
     private static String _message;
     
+    private static Logger logger = LogManager.getLogger(StatusUpdater.class);
+    
     private static void usage() {
         System.out.println( "usage: ( -ds <datasetID> | -k <submissionKey> ) [ -v <versionNum> ] -s status* [ -m <status message> ]\n" +
                             "Valid status states are:\n" + validStatusStatesList());
@@ -43,6 +53,51 @@ public class StatusUpdater {
             sb.append(state).append("\n");
         }
         return sb.toString();
+    }
+    
+    public static SubmissionRecord updateStatus(String datasetId, StatusState sstate, String message) 
+            throws NotFoundException, DashboardException {
+        try {
+            SubmissionRecord srec;
+            String notificationTitle = "Status update for " + datasetId;
+            String logMessage = "";
+            SubmissionsDao sdao = DaoFactory.SubmissionsDao();
+            if ( Archive.isRecordKey(datasetId)) {
+                srec = sdao.getLatestByKey(datasetId);
+            } else {
+                srec = sdao.getLatestForDataset(datasetId);
+            }
+            if ( srec == null ) {
+                notificationTitle = "FAILED: " + notificationTitle;
+                logMessage = "No submission record found.\n";
+                Notifications.AdminEmail(notificationTitle, logMessage);
+                throw new NotFoundException("No submission record found for dataset ID: " + datasetId);
+            } else {
+                StatusRecord currentState = srec.status();
+                Archive.updateStatus(srec, sstate, message);
+                logMessage = "The archive status for submission record " + datasetId +
+                        " has been changed from " + currentState.status() + ":" + currentState.message() +
+                        " to " + sstate + ":" + message;
+                if ( ApplicationConfiguration.getProperty("oap.archive.update.notify.user", false)) {
+                    try {
+                        User submitter = Users.getDataSubmitter(srec.datasetId());
+                        Notifications.SendEmail("Status updated for " + datasetId, logMessage, submitter.email());
+                    } catch (Exception ex) {
+                        logger.warn("Exception sending status update to submitter: " + ex , ex);
+                        StringBuilder exMsg = new StringBuilder(ex.toString()).append("\n");
+                        exMsg.append(DashboardServerUtils.stackTraceToString(ex.getStackTrace()));
+                        Notifications.AdminEmail("Exception sending status update message.", exMsg.toString());
+                    }
+                }
+            }
+            Notifications.AdminEmail(notificationTitle, logMessage);
+            return srec;
+        } catch (Exception ex) {
+            logger.warn(ex, ex);
+            String exceptionMsg = DashboardServerUtils.exceptionToString(ex);
+            Notifications.AdminEmail("Exception updating status for " + datasetId, exceptionMsg);
+            throw new DashboardException("Exception updating archive status for dataset " + datasetId, ex);
+        }
     }
     
     private static void parseArgs(String[] args) {
