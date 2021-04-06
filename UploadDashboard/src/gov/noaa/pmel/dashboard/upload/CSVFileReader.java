963bb13450113d53515ad52456d270a023093c94
linus.kamb <Linus.Kamb@noaa.gov>
2021-04-06 13:48:13 -0700

    Change to using File instead of InputStream for upload processing - in particular checking for strict Excel files - to deal with Google Drive-created xlsx files that put the data block before the worksheet block which blows out the BufferedInputStream mark.

--- UploadDashboard/src/gov/noaa/pmel/dashboard/upload/CSVFileReader.java
+++ UploadDashboard/src/gov/noaa/pmel/dashboard/upload/CSVFileReader.java
@@ -5,6 +5,8 @@ package gov.noaa.pmel.dashboard.upload;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Iterator;
@@ -34,6 +36,11 @@ public class CSVFileReader implements RecordOrientedFileReader {
         this.dataReader = new BufferedReader(new InputStreamReader(inputStream)); 
         this.detectedFileType = fileType;
     }
+    public CSVFileReader(File inputFile, String fileType) throws FileNotFoundException {
+        this.dataReader = new BufferedReader(new FileReader(inputFile)); 
+        this.detectedFileType = fileType;
+    }
+    
     
     
     /* (non-Javadoc)
